3ef9e1b983af2566843962b68213c1bbb4b04029
Rinor Hagmann <rinorhagmann@outlook.com>
2026-02-09 17:41:33 +0100

    add model and configure android emulator

---
+++ ParkhausMAUI/Models/ParkingModels.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkhausMAUI.Models
+{
+    // Parkhaus Daten
+    public class ParkingLocation
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double HourlyRate { get; set; }
+    }
+
+    // Parkvorgang-Daten
+    public class ParkingSession
+    {
+        public string ParkhausName { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public double TotalCost { get; set; }
+        public bool IsActive => EndTime == null;
+    }
+
+    // RootData für JSON-Daten
+    public class RootData
+    {
+        public List<ParkingLocation> AvailableParkings { get; set; } = new();
+        public List<ParkingSession> History { get; set; } = new();
+        public ParkingSession CurrentSession { get; set; } // Für den aktiven Parkvorgang
+    }
+}
